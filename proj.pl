bd264fd70f266ae30afa72ddf416298f5eb3b43e
Baltasar Dinis <baltasardinis@gmail.com>
2018-04-17 13:40:50 +0100

    Done verifica_parcial Done total_col, total_lin, sub_total_col Done aux functions: col, linha, elemento_k, dim

--- proj.pl
+++ proj.pl
@@ -8,9 +8,7 @@
 
 
 
-
-% propaga(Puz, Pos, Posicoes)
-%
+%------------------------ propaga(Puz, Pos, Posicoes) ------------------------%
 % preencher Pos implica preencher todos os elementos de Posicoes
 %
 % dependencias:
@@ -20,7 +18,8 @@
 
 propaga(Puz, Pos, Posicoes) :-
   term(Puz, Pos, Term),
-  propaga_aux(Pos, Term, Posicoes).
+  propaga_aux(Pos, Term, Unsorted),
+  sort(Unsorted, Posicoes).
 
 % constroi a lista de posicoes a preencher
 propaga_aux(Pos, [Pos|_], [Pos]).
@@ -39,9 +38,96 @@ term_aux([Term|_], Pos, Term) :- isin(Pos, Term).
 term_aux([_|T], Pos, Term) :- term_aux(T, Pos, Term).
 % senao existir, retorna falso
 
+
+
+%---------- nao_altera_linhas_anteriores(Posicoes, J, Ja_Preenchidas) ---------%
+% Se preenchermos a linha L com Posicoes, todas as posicoes da lista que pertencem
+% a linhas anteriores a L pertencem a Ja_Preenchidas
+%
+% dependencias: isin
+
+nao_altera_linhas_anteriores([], _, _).
+nao_altera_linhas_anteriores([(I, J)|T], N, Ja_Preenchidas) :-
+  I < N,
+  isin((I, J), Ja_Preenchidas),
+  nao_altera_linhas_anteriores(T, N, Ja_Preenchidas).
+
+nao_altera_linhas_anteriores([(I, _)|T], N, Ja_Preenchidas) :-
+  I >= N,
+  nao_altera_linhas_anteriores(T, N, Ja_Preenchidas).
+
+
+%---------- verifica_parcial(Puz, Ja_Preenchidas, Dim, Poss) ---------%
+% Dado o puzzle: Puz; com dimensao: Dim
+% Dada a lista de posições ja preenchidas: Ja_Preenchidas
+% Dada a lista de possibilidades a preencher para a linha N: Poss
+%
+% dependencias: 
+%   * total_col, sub_total_col, col
+%     ** elemento_k
+
+verifica_parcial(_, _, _, []).
+verifica_parcial(Puz, Ja_Preenchidas, Dim, [H|T]) :-
+  col(H, J),
+  total_col(Puz, J, Total),
+  sub_total_col(J, Ja_Preenchidas, SubTotal),
+  Parc is SubTotal + 1,
+  Parc =< Total,
+  verifica_parcial(Puz, Ja_Preenchidas, Dim, T).
+
+% extrai do puzzle o total da linha J
+total_col([_, _, Totais_Colunas], J, Total) :-
+  length(Totais_Colunas, Dim),
+  J =< Dim,
+  elemento_k(Totais_Colunas, J, Total).
+
+% calcula o sub_total da coluna J, dada a lista de posicoes preenchidas
+sub_total_col(_, [], 0).
+sub_total_col(J, [H|T], STotal) :- 
+  col(H, JPos),
+  JPos == J,
+  sub_total_col(J, T, Parc),
+  STotal is Parc + 1.
+
+sub_total_col(J, [H|T], STotal) :- 
+  col(H, JPos),
+  JPos \= J,
+  sub_total_col(J, T, Parc),
+  STotal is Parc.
+  
+% extrai do puzzle o total da linha I
+total_lin([_, Totais_Linhas, _], I, Total) :-
+  length(Totais_Linhas, Dim),
+  I =< Dim,
+  elemento_k(Totais_Linhas, I, Total).
+
+
+
+
+%---------- funcoes auxiliares -----------%
+% isin(Termo, Lista)
 % se esta na cabeça retorna,
 % senao, procura no resto da lista
-isin(Pos, [Pos|_]).
-isin(Pos, [H|T]) :- 
-  \+ Pos == H,
-  isin(Pos, T).
+isin(Termo, [Termo|_]).
+isin(Termo, [H|T]) :- 
+  Termo \= H,
+  isin(Termo, T).
+
+% linha(Posicao, Linha): encontra a linha (1o elemento) de um tuplo
+linha((I, _), I). 
+
+% col(Posicao, Coluna): encontra a coluna (2o elemento) de um tuplo
+col((_, J), J). 
+
+% elemento_k(Lista, Pos, El): encontra o elemento da Lista na posicao Pos 
+elemento_k([El|_], 1, El).
+elemento_k([_|T], Pos, El) :-
+  Pos > 1,
+  NPos is Pos -1,
+  elemento_k(T, NPos, El).
+
+% encontra a dimensão do puzzle
+% Dim Puzzle[Pos, Linhas, Colunas] = Dim Linhas
+dim([_, Linhas, _], Len) :- length(Linhas, Len).
+
+
